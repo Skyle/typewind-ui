4ee092e67698efebabd3c854867b1bbf0c6123a0
Philipp Friedrich <freezer05@googlemail.com>
2022-03-12 08:59:37 +0100

    red and green button

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import Button from "./components/UI/Button";
 function App() {
   return (
     <div>
-      <header className="text-center pb-8">
+      <header className="text-center pb-8 grid justify-center">
         <h1 className="text-5xl font-extralight py-4">Typewind UI</h1>
-        <h2 className="text-xl font-light pb-2">React UI Component Library</h2>
+        <div className="relative">
+          <h2 className="text-xl font-light pb-2">
+            React UI Component Exhibition
+          </h2>
+          <div className="absolute -right-24 -top-1  text-sm font-extralight">
+            * maybe Library
+          </div>
+        </div>
+
         <div className="font-light">
           <ul className="">
             <li>minimalistic styling with tailwindcss</li>
@@ -16,6 +24,7 @@ function App() {
       <main className="px-2 flex space-x-2">
         <Button></Button>
         <Button color="green"></Button>
+        <Button color="red"></Button>
       </main>
     </div>
   );
--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -10,9 +10,10 @@ function Button({ children, onClick, color }: Props) {
   return (
     <button
       className={
-        "appearance-none select-none shadow dark:shadow-none rounded border px-4 py-1 bg text" +
-        (color === "green" ? "" : "") +
-        (color === undefined ? " " : "")
+        "button-base" +
+        (color === "green" ? " border-green bg-green" : "") +
+        (color === "red" ? " border-red bg-red" : "") +
+        (color === undefined ? " border-neutral bg-neutral" : "")
       }
       onClick={(e) => {
         if (onClick) onClick(e);
