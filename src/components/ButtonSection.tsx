82a6654a363aeff817a0c0f90584ab124e0cb478
Philipp Friedrich <freezer05@googlemail.com>
2022-03-22 09:07:04 +0100

    button rewrite

--- src/components/ButtonSection.tsx
+++ src/components/ButtonSection.tsx
@@ -7,21 +7,50 @@ type Props = {};
 function ButtonSection({}: Props) {
   const [disabletest, setDisabletest] = useState(false);
   const [fat, setFat] = useState(false);
+  const [globalDisable, setGlobalDisable] = useState(false);
   const [shadow, setShadow] = useState(true);
+  const [clickCounter, setClickCounter] = useState(0);
   return (
     <section className="">
       <div className="grid gap-4 md:grid-cols-2 max-w-4xl">
         <div className="">
           <h3 className="pb-2">Buttons</h3>
           <div className="flex flex-wrap">
-            <Button shadow={shadow}>Klick Mich</Button>
-            <Button color="green" shadow={shadow}></Button>
-            <Button color="blue" shadow={shadow}></Button>
+            <Button
+              onClick={() => {
+                setClickCounter(clickCounter + 1);
+              }}
+              shadow={shadow}
+              disabled={globalDisable}
+            >
+              Klick Mich
+            </Button>
+            <Button
+              color="green"
+              onClick={() => {
+                setClickCounter(clickCounter + 1);
+              }}
+              disabled={globalDisable}
+              shadow={shadow}
+            ></Button>
+            <Button
+              color="blue"
+              onClick={() => {
+                setClickCounter(clickCounter + 1);
+              }}
+              shadow={shadow}
+              disabled={globalDisable}
+            ></Button>
             <Button disabled={true}></Button>
             <Button
-              disabled={disabletest}
+              disabled={disabletest || globalDisable}
               onClick={() => {
                 setDisabletest(true);
+                if (clickCounter < 0) {
+                  setClickCounter(clickCounter - 20);
+                } else {
+                  setClickCounter(-20);
+                }
                 setTimeout(() => {
                   setDisabletest(false);
                 }, 1000);
@@ -41,14 +70,27 @@ function ButtonSection({}: Props) {
           </div>
           <div>
             <Checkbox
-              label="Schatten"
+              label="shadow"
               onChange={(e) => {
-                console.log(e.target.checked);
-
                 setShadow(e.target.checked);
               }}
               checked={shadow}
             ></Checkbox>
+          </div>
+          <div>
+            <Checkbox
+              label="disabled"
+              onChange={(e) => {
+                setGlobalDisable(e.target.checked);
+              }}
+              checked={globalDisable}
+            ></Checkbox>
+          </div>
+          <div className="font-extralight">
+            {clickCounter !== 0 && clickCounter !== 1 && (
+              <div>{clickCounter} Klicks</div>
+            )}
+            {clickCounter === 1 && <div>Dein erster Klick!</div>}
           </div>
         </div>
       </div>
