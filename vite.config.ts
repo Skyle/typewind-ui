20b4e01dcd23a4f20c983bf72b3a0b5e2b9cce4f
Philipp Friedrich <freezer05@googlemail.com>
2022-04-03 07:47:59 +0200

    asdfasdf

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/components/index.js"),
+      name: "MyLib",
+      fileName: (format) => `my-lib.${format}.js`,
+    },
+  },
+  plugins: [react()],
+});
